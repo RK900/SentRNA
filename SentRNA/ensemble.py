06ebecc29a668284cfca11fc48c1d6652b7e9887
Rohan Koodli <rovik05@gmail.com>
2019-10-08 12:48:22 -0700

    Use os.system for consistency

--- SentRNA/ensemble.py
+++ SentRNA/ensemble.py
@@ -1,8 +1,8 @@
-import subprocess
+import os
 
 for i in range(20):
-    subprocess.check_output('python', 'run.py', '--mode' 'train', '--input_data', '../data/train/eterna_complete_ss.pkl', '--results_path', 'model%i' % i, '--n_long_range_features', '20')
+    os.system('python run.py --mode train --input_data ../data/train/eterna_complete_ss.pkl --results_path model%i --n_long_range_features 20' % i)
     # Test test
-    subprocess.check_output('python', 'run.py', '--mode', 'test', '--input_data', '../data/test/eterna100.pkl', '--test_model', 'test/model%i' % i, '--results_path', 'model_tested%i.pkl' % i)
+    os.system('python run.py --mode test --input_data ../data/test/eterna100.pkl --test_model test/model%i --results_path model_tested%i.pkl' % (i, i))
     # Refine test
-    subprocess.check_output('python', 'run.py', '--mode', 'refine', '--input_data', 'test_results/model_tested%i.pkl' % i, '--results_path', 'model_refined%i.pkl' % i)
+    os.system('python run.py --mode refine --input_data test_results/model_tested%i.pkl --results_path model_refined%i.pkl' % (i, i))
